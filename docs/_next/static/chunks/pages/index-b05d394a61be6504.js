975d56e94bac6fd9d25f04053e2e78eefcda0ea5
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-07-04 18:48:19 +0700

    update navbar

--- docs/_next/static/chunks/pages/index-b05d394a61be6504.js
+++ docs/_next/static/chunks/pages/index-b05d394a61be6504.js
@@ -8,7 +8,7 @@
 					for (var r in t) Object.defineProperty(e, r, { enumerable: !0, get: t[r] });
 				})(t, {
 					default: function () {
-						return x;
+						return _;
 					},
 					useLinkStatus: function () {
 						return j;
@@ -16,25 +16,25 @@
 				}));
 			let n = r(8781),
 				o = r(5640),
-				u = n._(r(148)),
-				l = r(2480),
+				l = n._(r(148)),
+				u = r(2480),
 				a = r(2402),
 				s = r(6939),
 				i = r(9925),
 				c = r(5686),
-				f = r(3295),
-				d = r(7233),
+				d = r(3295),
+				f = r(7233),
 				p = r(4434),
 				h = r(5767),
 				m = r(6492);
 			r(5446);
-			let b = new Set();
-			function g(e, t, r, n) {
+			let g = new Set();
+			function b(e, t, r, n) {
 				if ((0, a.isLocalURL)(t)) {
 					if (!n.bypassPrefetchedCheck) {
 						let o = t + '%' + r + '%' + (void 0 !== n.locale ? n.locale : 'locale' in e ? e.locale : void 0);
-						if (b.has(o)) return;
-						b.add(o);
+						if (g.has(o)) return;
+						g.add(o);
 					}
 					e.prefetch(t, r, n).catch((e) => {});
 				}
@@ -42,60 +42,60 @@
 			function v(e) {
 				return 'string' == typeof e ? e : (0, s.formatUrl)(e);
 			}
-			let y = u.default.forwardRef(function (e, t) {
+			let y = l.default.forwardRef(function (e, t) {
 					let r,
 						n,
 						{
 							href: s,
-							as: b,
+							as: g,
 							children: y,
-							prefetch: _ = null,
+							prefetch: x = null,
 							passHref: j,
-							replace: x,
+							replace: _,
 							shallow: M,
-							scroll: O,
-							locale: P,
-							onClick: k,
-							onNavigate: w,
+							scroll: w,
+							locale: O,
+							onClick: P,
+							onNavigate: k,
 							onMouseEnter: C,
-							onTouchStart: E,
-							legacyBehavior: N = !1,
+							onTouchStart: N,
+							legacyBehavior: E = !1,
 							...R
 						} = e;
-					((r = y), N && ('string' == typeof r || 'number' == typeof r) && (r = (0, o.jsx)('a', { children: r })));
-					let L = u.default.useContext(f.RouterContext),
-						I = !1 !== _,
-						{ href: S, as: T } = u.default.useMemo(() => {
+					((r = y), E && ('string' == typeof r || 'number' == typeof r) && (r = (0, o.jsx)('a', { children: r })));
+					let L = l.default.useContext(d.RouterContext),
+						I = !1 !== x,
+						{ href: S, as: T } = l.default.useMemo(() => {
 							if (!L) {
 								let e = v(s);
-								return { href: e, as: b ? v(b) : e };
+								return { href: e, as: g ? v(g) : e };
 							}
-							let [e, t] = (0, l.resolveHref)(L, s, !0);
-							return { href: e, as: b ? (0, l.resolveHref)(L, b) : t || e };
-						}, [L, s, b]),
-						D = u.default.useRef(S),
-						A = u.default.useRef(T);
-					N && (n = u.default.Children.only(r));
-					let U = N ? n && 'object' == typeof n && n.ref : t,
-						[K, H, X] = (0, d.useIntersection)({ rootMargin: '200px' }),
-						q = u.default.useCallback(
+							let [e, t] = (0, u.resolveHref)(L, s, !0);
+							return { href: e, as: g ? (0, u.resolveHref)(L, g) : t || e };
+						}, [L, s, g]),
+						A = l.default.useRef(S),
+						D = l.default.useRef(T);
+					E && (n = l.default.Children.only(r));
+					let U = E ? n && 'object' == typeof n && n.ref : t,
+						[K, H, X] = (0, f.useIntersection)({ rootMargin: '200px' }),
+						q = l.default.useCallback(
 							(e) => {
-								((A.current !== T || D.current !== S) && (X(), (A.current = T), (D.current = S)), K(e));
+								((D.current !== T || A.current !== S) && (X(), (D.current = T), (A.current = S)), K(e));
 							},
 							[T, S, X, K],
 						),
 						z = (0, m.useMergedRef)(q, U);
-					u.default.useEffect(() => {
-						L && H && I && g(L, S, T, { locale: P });
-					}, [T, S, H, P, I, null == L ? void 0 : L.locale, L]);
+					l.default.useEffect(() => {
+						L && H && I && b(L, S, T, { locale: O });
+					}, [T, S, H, O, I, null == L ? void 0 : L.locale, L]);
 					let B = {
 						ref: z,
 						onClick(e) {
-							(N || 'function' != typeof k || k(e),
-								N && n.props && 'function' == typeof n.props.onClick && n.props.onClick(e),
+							(E || 'function' != typeof P || P(e),
+								E && n.props && 'function' == typeof n.props.onClick && n.props.onClick(e),
 								L &&
 									(e.defaultPrevented ||
-										(function (e, t, r, n, o, u, l, s, i) {
+										(function (e, t, r, n, o, l, u, s, i) {
 											let { nodeName: c } = e.currentTarget;
 											if (
 												!(
@@ -132,38 +132,38 @@
 															)
 																return;
 														}
-														let e = null == l || l;
+														let e = null == u || u;
 														'beforePopState' in t
-															? t[o ? 'replace' : 'push'](r, n, { shallow: u, locale: s, scroll: e })
+															? t[o ? 'replace' : 'push'](r, n, { shallow: l, locale: s, scroll: e })
 															: t[o ? 'replace' : 'push'](n || r, { scroll: e });
 													})());
 											}
-										})(e, L, S, T, x, M, O, P, w)));
+										})(e, L, S, T, _, M, w, O, k)));
 						},
 						onMouseEnter(e) {
-							(N || 'function' != typeof C || C(e),
-								N && n.props && 'function' == typeof n.props.onMouseEnter && n.props.onMouseEnter(e),
-								L && g(L, S, T, { locale: P, priority: !0, bypassPrefetchedCheck: !0 }));
+							(E || 'function' != typeof C || C(e),
+								E && n.props && 'function' == typeof n.props.onMouseEnter && n.props.onMouseEnter(e),
+								L && b(L, S, T, { locale: O, priority: !0, bypassPrefetchedCheck: !0 }));
 						},
 						onTouchStart: function (e) {
-							(N || 'function' != typeof E || E(e),
-								N && n.props && 'function' == typeof n.props.onTouchStart && n.props.onTouchStart(e),
-								L && g(L, S, T, { locale: P, priority: !0, bypassPrefetchedCheck: !0 }));
+							(E || 'function' != typeof N || N(e),
+								E && n.props && 'function' == typeof n.props.onTouchStart && n.props.onTouchStart(e),
+								L && b(L, S, T, { locale: O, priority: !0, bypassPrefetchedCheck: !0 }));
 						},
 					};
 					if ((0, i.isAbsoluteUrl)(T)) B.href = T;
-					else if (!N || j || ('a' === n.type && !('href' in n.props))) {
-						let e = void 0 !== P ? P : null == L ? void 0 : L.locale;
+					else if (!E || j || ('a' === n.type && !('href' in n.props))) {
+						let e = void 0 !== O ? O : null == L ? void 0 : L.locale;
 						B.href =
 							((null == L ? void 0 : L.isLocaleDomain) &&
 								(0, p.getDomainLocale)(T, e, null == L ? void 0 : L.locales, null == L ? void 0 : L.domainLocales)) ||
 							(0, h.addBasePath)((0, c.addLocale)(T, e, null == L ? void 0 : L.defaultLocale));
 					}
-					return N ? u.default.cloneElement(n, B) : (0, o.jsx)('a', { ...R, ...B, children: r });
+					return E ? l.default.cloneElement(n, B) : (0, o.jsx)('a', { ...R, ...B, children: r });
 				}),
-				_ = (0, u.createContext)({ pending: !1 }),
-				j = () => (0, u.useContext)(_),
-				x = y;
+				x = (0, l.createContext)({ pending: !1 }),
+				j = () => (0, l.useContext)(x),
+				_ = y;
 			('function' == typeof t.default || ('object' == typeof t.default && null !== t.default)) &&
 				void 0 === t.default.__esModule &&
 				(Object.defineProperty(t.default, '__esModule', { value: !0 }),
@@ -172,79 +172,100 @@
 		},
 		2128: (e, t, r) => {
 			'use strict';
-			(r.r(t), r.d(t, { default: () => l }));
+			(r.r(t), r.d(t, { default: () => u }));
 			var n = r(5640),
 				o = r(7864),
-				u = r.n(o);
-			let l = () =>
+				l = r.n(o);
+			let u = () =>
 				(0, n.jsxs)(n.Fragment, {
 					children: [
 						(0, n.jsx)('nav', {
 							className: 'border-b border-neutral-200 shadow-sm',
 							children: (0, n.jsx)('div', {
 								className: 'container mx-auto px-4 py-2 md:px-8 md:py-4',
-								children: (0, n.jsx)('h1', { className: 'font-black', children: 'Mini App Store' }),
+								children: (0, n.jsxs)('div', {
+									className: 'flex items-center justify-between',
+									children: [
+										(0, n.jsx)('h1', { className: 'font-black', children: 'Store' }),
+										(0, n.jsx)('div', {
+											className: 'flex items-center gap-x-2 md:gap-x-4',
+											children: (0, n.jsx)(l(), { href: '/', children: 'Mini Apps' }),
+										}),
+									],
+								}),
 							}),
 						}),
 						(0, n.jsx)('div', {
 							className: 'container mx-auto p-4 md:p-8',
-							children: (0, n.jsx)('div', {
-								className: 'grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3',
+							children: (0, n.jsxs)('div', {
+								className: 'flex flex-col gap-y-4 md:gap-y-8',
 								children: [
-									{
-										id: 'chess',
-										href: 'https://hieudoanm.github.io/chess',
-										image: 'https://raw.githubusercontent.com/hieudoanm/chess/refs/heads/master/images/cover.png',
-										name: 'chess',
-									},
-									{
-										id: 'numbers',
-										href: 'https://hieudoanm.github.io/numbers',
-										image: 'https://raw.githubusercontent.com/hieudoanm/numbers/refs/heads/master/images/cover.png',
-										name: 'numbers',
-									},
-									{
-										id: 're(d)act',
-										href: 'https://hieudoanm.github.io/redact',
-										image: 'https://raw.githubusercontent.com/hieudoanm/redact/refs/heads/master/images/cover.png',
-										name: 're(d)act',
-									},
-									{
-										id: 'status',
-										href: 'https://hieudoanm.github.io/status',
-										image: 'https://raw.githubusercontent.com/hieudoanm/status/refs/heads/master/images/cover.png',
-										name: 'status',
-									},
-									{
-										id: 'strings',
-										href: 'https://hieudoanm.github.io/strings',
-										image: 'https://raw.githubusercontent.com/hieudoanm/strings/refs/heads/master/images/cover.png',
-										name: 'strings',
-									},
-								].map((e) => {
-									let { id: t = '', href: r = '', image: o = '', name: l = '' } = e;
-									return (0, n.jsx)(
-										u(),
-										{
-											href: r,
-											target: '_blank',
-											children: (0, n.jsxs)('div', {
-												className: 'overflow-hidden rounded-2xl border border-neutral-200 shadow',
-												children: [
-													(0, n.jsx)('div', {
-														className: 'aspect-video bg-contain bg-center bg-no-repeat',
-														style: { backgroundImage: 'url('.concat(o, ')') },
+									(0, n.jsx)('h2', { className: 'text-2xl font-extrabold md:text-3xl', children: 'Mini Apps' }),
+									(0, n.jsx)('div', {
+										className: 'grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3',
+										children: [
+											{
+												id: 'chess',
+												href: 'https://hieudoanm.github.io/chess',
+												image: 'https://raw.githubusercontent.com/hieudoanm/chess/refs/heads/master/images/cover.png',
+												name: 'chess',
+											},
+											{
+												id: 'csv',
+												href: 'https://hieudoanm.github.io/csv',
+												image: 'https://raw.githubusercontent.com/hieudoanm/csv/refs/heads/master/images/cover.png',
+												name: 'csv',
+											},
+											{
+												id: 'numbers',
+												href: 'https://hieudoanm.github.io/numbers',
+												image: 'https://raw.githubusercontent.com/hieudoanm/numbers/refs/heads/master/images/cover.png',
+												name: 'numbers',
+											},
+											{
+												id: 're(d)act',
+												href: 'https://hieudoanm.github.io/redact',
+												image: 'https://raw.githubusercontent.com/hieudoanm/redact/refs/heads/master/images/cover.png',
+												name: 're(d)act',
+											},
+											{
+												id: 'status',
+												href: 'https://hieudoanm.github.io/status',
+												image: 'https://raw.githubusercontent.com/hieudoanm/status/refs/heads/master/images/cover.png',
+												name: 'status',
+											},
+											{
+												id: 'strings',
+												href: 'https://hieudoanm.github.io/strings',
+												image: 'https://raw.githubusercontent.com/hieudoanm/strings/refs/heads/master/images/cover.png',
+												name: 'strings',
+											},
+										].map((e) => {
+											let { id: t = '', href: r = '', image: o = '', name: u = '' } = e;
+											return (0, n.jsx)(
+												l(),
+												{
+													href: r,
+													target: '_blank',
+													children: (0, n.jsxs)('div', {
+														className: 'overflow-hidden rounded-2xl border border-neutral-200 shadow',
+														children: [
+															(0, n.jsx)('div', {
+																className: 'aspect-video bg-contain bg-center bg-no-repeat',
+																style: { backgroundImage: 'url('.concat(o, ')') },
+															}),
+															(0, n.jsx)('div', {
+																className: 'border-t border-neutral-200 p-4',
+																children: (0, n.jsx)('h4', { className: 'text-lg font-black md:text-xl', children: u }),
+															}),
+														],
 													}),
-													(0, n.jsx)('div', {
-														className: 'border-t border-neutral-200 p-4',
-														children: (0, n.jsx)('h4', { className: 'text-lg font-black md:text-xl', children: l }),
-													}),
-												],
-											}),
-										},
-										t,
-									);
-								}),
+												},
+												t,
+											);
+										}),
+									}),
+								],
 							}),
 						}),
 					],
@@ -300,12 +321,12 @@
 							e && ((r.current = null), e());
 							let t = o.current;
 							t && ((o.current = null), t());
-						} else (e && (r.current = u(e, n)), t && (o.current = u(t, n)));
+						} else (e && (r.current = l(e, n)), t && (o.current = l(t, n)));
 					},
 					[e, t],
 				);
 			}
-			function u(e, t) {
+			function l(e, t) {
 				if ('function' != typeof e)
 					return (
 						(e.current = t),
@@ -335,33 +356,33 @@
 				}));
 			let n = r(148),
 				o = r(9780),
-				u = 'function' == typeof IntersectionObserver,
-				l = new Map(),
+				l = 'function' == typeof IntersectionObserver,
+				u = new Map(),
 				a = [];
 			function s(e) {
 				let { rootRef: t, rootMargin: r, disabled: s } = e,
-					i = s || !u,
-					[c, f] = (0, n.useState)(!1),
-					d = (0, n.useRef)(null),
+					i = s || !l,
+					[c, d] = (0, n.useState)(!1),
+					f = (0, n.useRef)(null),
 					p = (0, n.useCallback)((e) => {
-						d.current = e;
+						f.current = e;
 					}, []);
 				return (
 					(0, n.useEffect)(() => {
-						if (u) {
+						if (l) {
 							if (i || c) return;
-							let e = d.current;
+							let e = f.current;
 							if (e && e.tagName)
 								return (function (e, t, r) {
 									let {
 										id: n,
 										observer: o,
-										elements: u,
+										elements: l,
 									} = (function (e) {
 										let t,
 											r = { root: e.root || null, margin: e.rootMargin || '' },
 											n = a.find((e) => e.root === r.root && e.margin === r.margin);
-										if (n && (t = l.get(n))) return t;
+										if (n && (t = u.get(n))) return t;
 										let o = new Map();
 										return (
 											(t = {
@@ -376,32 +397,32 @@
 												elements: o,
 											}),
 											a.push(r),
-											l.set(r, t),
+											u.set(r, t),
 											t
 										);
 									})(r);
 									return (
-										u.set(e, t),
+										l.set(e, t),
 										o.observe(e),
 										function () {
-											if ((u.delete(e), o.unobserve(e), 0 === u.size)) {
-												(o.disconnect(), l.delete(n));
+											if ((l.delete(e), o.unobserve(e), 0 === l.size)) {
+												(o.disconnect(), u.delete(n));
 												let e = a.findIndex((e) => e.root === n.root && e.margin === n.margin);
 												e > -1 && a.splice(e, 1);
 											}
 										}
 									);
-								})(e, (e) => e && f(e), { root: null == t ? void 0 : t.current, rootMargin: r });
+								})(e, (e) => e && d(e), { root: null == t ? void 0 : t.current, rootMargin: r });
 						} else if (!c) {
-							let e = (0, o.requestIdleCallback)(() => f(!0));
+							let e = (0, o.requestIdleCallback)(() => d(!0));
 							return () => (0, o.cancelIdleCallback)(e);
 						}
-					}, [i, r, t, c, d.current]),
+					}, [i, r, t, c, f.current]),
 					[
 						p,
 						c,
 						(0, n.useCallback)(() => {
-							f(!1);
+							d(!1);
 						}, []),
 					]
 				);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,61 +2,77 @@ import { NextPage } from 'next';
 import Link from 'next/link';
 
 const HomePage: NextPage = () => {
+	const miniApps = [
+		{
+			id: 'chess',
+			href: 'https://hieudoanm.github.io/chess',
+			image: 'https://raw.githubusercontent.com/hieudoanm/chess/refs/heads/master/images/cover.png',
+			name: 'chess',
+		},
+		{
+			id: 'csv',
+			href: 'https://hieudoanm.github.io/csv',
+			image: 'https://raw.githubusercontent.com/hieudoanm/csv/refs/heads/master/images/cover.png',
+			name: 'csv',
+		},
+		{
+			id: 'numbers',
+			href: 'https://hieudoanm.github.io/numbers',
+			image: 'https://raw.githubusercontent.com/hieudoanm/numbers/refs/heads/master/images/cover.png',
+			name: 'numbers',
+		},
+		{
+			id: 're(d)act',
+			href: 'https://hieudoanm.github.io/redact',
+			image: 'https://raw.githubusercontent.com/hieudoanm/redact/refs/heads/master/images/cover.png',
+			name: 're(d)act',
+		},
+		{
+			id: 'status',
+			href: 'https://hieudoanm.github.io/status',
+			image: 'https://raw.githubusercontent.com/hieudoanm/status/refs/heads/master/images/cover.png',
+			name: 'status',
+		},
+		{
+			id: 'strings',
+			href: 'https://hieudoanm.github.io/strings',
+			image: 'https://raw.githubusercontent.com/hieudoanm/strings/refs/heads/master/images/cover.png',
+			name: 'strings',
+		},
+	];
+
 	return (
 		<>
 			<nav className="border-b border-neutral-200 shadow-sm">
 				<div className="container mx-auto px-4 py-2 md:px-8 md:py-4">
-					<h1 className="font-black">Mini App Store</h1>
+					<div className="flex items-center justify-between">
+						<h1 className="font-black">Store</h1>
+						<div className="flex items-center gap-x-2 md:gap-x-4">
+							<Link href="/">Mini Apps</Link>
+						</div>
+					</div>
 				</div>
 			</nav>
 			<div className="container mx-auto p-4 md:p-8">
-				<div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
-					{[
-						{
-							id: 'chess',
-							href: 'https://hieudoanm.github.io/chess',
-							image: 'https://raw.githubusercontent.com/hieudoanm/chess/refs/heads/master/images/cover.png',
-							name: 'chess',
-						},
-						{
-							id: 'numbers',
-							href: 'https://hieudoanm.github.io/numbers',
-							image: 'https://raw.githubusercontent.com/hieudoanm/numbers/refs/heads/master/images/cover.png',
-							name: 'numbers',
-						},
-						{
-							id: 're(d)act',
-							href: 'https://hieudoanm.github.io/redact',
-							image: 'https://raw.githubusercontent.com/hieudoanm/redact/refs/heads/master/images/cover.png',
-							name: 're(d)act',
-						},
-						{
-							id: 'status',
-							href: 'https://hieudoanm.github.io/status',
-							image: 'https://raw.githubusercontent.com/hieudoanm/status/refs/heads/master/images/cover.png',
-							name: 'status',
-						},
-						{
-							id: 'strings',
-							href: 'https://hieudoanm.github.io/strings',
-							image: 'https://raw.githubusercontent.com/hieudoanm/strings/refs/heads/master/images/cover.png',
-							name: 'strings',
-						},
-					].map(({ id = '', href = '', image = '', name = '' }) => {
-						return (
-							<Link key={id} href={href} target="_blank">
-								<div className="overflow-hidden rounded-2xl border border-neutral-200 shadow">
-									<div
-										className="aspect-video bg-contain bg-center bg-no-repeat"
-										style={{ backgroundImage: `url(${image})` }}
-									/>
-									<div className="border-t border-neutral-200 p-4">
-										<h4 className="text-lg font-black md:text-xl">{name}</h4>
+				<div className="flex flex-col gap-y-4 md:gap-y-8">
+					<h2 className="text-2xl font-extrabold md:text-3xl">Mini Apps</h2>
+					<div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
+						{miniApps.map(({ id = '', href = '', image = '', name = '' }) => {
+							return (
+								<Link key={id} href={href} target="_blank">
+									<div className="overflow-hidden rounded-2xl border border-neutral-200 shadow">
+										<div
+											className="aspect-video bg-contain bg-center bg-no-repeat"
+											style={{ backgroundImage: `url(${image})` }}
+										/>
+										<div className="border-t border-neutral-200 p-4">
+											<h4 className="text-lg font-black md:text-xl">{name}</h4>
+										</div>
 									</div>
-								</div>
-							</Link>
-						);
-					})}
+								</Link>
+							);
+						})}
+					</div>
 				</div>
 			</div>
 		</>
